84fab42a875c5f7ff3237a573ec5082e03ddc162
Florian Dorau <fdorau@it-economics.de>
2018-09-13 21:27:28 +0200

    Refactored ts-factory

---
+++ src/format-transformer/index.js
@@ -0,0 +1,22 @@
+const log = require('../logger.js');
+
+const xmlFormatTransformer = require('./xml-transfromer');
+const jsonFormatTransformer = require('./json-transformer');
+
+const formatTransformer = {
+    'json': jsonFormatTransformer,
+    'xml':xmlFormatTransformer
+};
+
+module.exports = {
+    transformerForFormat: (format) => {
+        log.debug(`Find transformer for format ${format}`);
+        
+        const transformer = formatTransformer[format];
+        if(!transformer) {
+            throw new Error(`Format ${format} not supported`);
+        }
+
+        return transformer;
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+const log = require('./logger');
+const tsFactory = require('./factory');
+const formatTransformer = require('./format-transformer');
 
+const stream = (options) => {
+    log.info(`ts-factory stream for ${options}`);
+    
+    const start = options.start;
+    const end = options.end;
+    const format = options.format || 'json';
+    const dstMode = options.dst || '2424';
+    const granulartiy = options.granulartiy || 'HOURLY';
 
-// TODO Implemnt factory api
-// required data is 
-const options = {
-    start: "2018-08-01",
-    end: "2018-09-01",
-    format: "xml",
-    granularity: "hourly",
+    const tsStream = tsFactory.stream(start, end, { granulartiy, dstMode });
+    const transformer = formatTransformer.transformerForFormat(format);
+
+    return tsStream.pipe(transformer.stream());
 };
 
-// Merge creater and writer
+const testFn = (opt) => stream(opt).on('data', (data) => console.log(data));
+testFn({
+    start: '2018-08-01',
+    end: '2018-09-01',
+    format: 'json',
+    granularity: 'hourly'
+});
+
+module.exports = { stream };
